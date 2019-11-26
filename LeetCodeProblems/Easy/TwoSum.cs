e9782a3cf9e80d8a860f92fa454234f3cdaff302
Jon Rice <revyolution1120@gmail.com>
2019-11-25 16:01:53 -0800

    Add project files.

---
+++ LeetCodeProblems/Easy/TwoSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Easy
+{
+    class TwoSum
+    {
+        public int[] TwoSumMethod(int[] nums, int target)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[2];
+        }
+    }
+}
